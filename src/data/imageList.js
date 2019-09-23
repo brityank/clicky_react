3b7f531f5b9d28d2889d619844d7b52eac159779
Aaron Keisler <amkeisler@gmail.com>
2019-09-23 09:22:49 -0400

    add image data

---
+++ src/data/imageList.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    src: "https://media.giphy.com/media/4qK1idIATjdAY/giphy.gif",
+    alt: "archer1",
+    key: 0
+  },
+  {
+    src: "http://giphygifs.s3.amazonaws.com/media/BUlwrDd97bCYE/giphy.gif",
+    alt: "archer2",
+    key: 1
+  },
+  {
+    src: "https://media.giphy.com/media/z75OjWknCTI6k/giphy.gif",
+    alt: "archer4",
+    key: 3
+  },
+  {
+    src: "http://giphygifs.s3.amazonaws.com/media/n0SYZLVDpJ1ZK/giphy.gif",
+    alt: "archer5",
+    key: 4
+  },
+  {
+    src: "http://giphygifs.s3.amazonaws.com/media/IIwCyoiNQjLpK/giphy.gif",
+    alt: "archer6",
+    key: 5
+  },
+  {
+    src: "https://media.giphy.com/media/pppjP3Qzp5a5q/giphy.gif",
+    alt: "archer7",
+    key: 6
+  },
+  {
+    src: "http://giphygifs.s3.amazonaws.com/media/Kz420G0aGw5mU/giphy.gif",
+    alt: "archer8",
+    key: 7
+  },
+  {
+    src: "http://giphygifs.s3.amazonaws.com/media/6pxG2dThniE5G/giphy.gif",
+    alt: "archer9",
+    key: 8
+  },
+  {
+    src: "http://giphygifs.s3.amazonaws.com/media/BLpLZukyvFVMk/giphy.gif",
+    alt: "archer10",
+    key: 9
+  },
+  {
+    src: "https://media.giphy.com/media/ZX8hXxYGWR2rS/giphy.gif",
+    alt: "archer11",
+    key: "10"
+  },
+  {
+    src: "https://media.giphy.com/media/i1oN9LiKwGmA/giphy.gif",
+    alt: "archer12",
+    key: "11"
+  },
+  {
+    src: "https://media.giphy.com/media/XlP95KL6sp4B2/giphy.gif",
+    alt: "archer13",
+    key: "12"
+  }
+];
