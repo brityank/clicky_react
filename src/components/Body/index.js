af3868d9143d1f8b0a217ee4a1a5224bf8e1f10d
Aaron Keisler <amkeisler@gmail.com>
2019-09-25 21:38:07 -0400

    delete boilerplate code

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Jumbotron, Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./style.css";
 
 export default class Body extends Component {
@@ -7,16 +7,7 @@ export default class Body extends Component {
     const { children } = this.props;
     return (
       <div>
-        <Container fluid>
-          <Jumbotron>
-            <h1>Clicky Game!</h1>
-            <p>This is a simple react memory game, using bootstrap components.</p>
-            <p>
-              <Button variant="primary">Start Game</Button>
-            </p>
-          </Jumbotron>
-          {children}
-        </Container>
+        <Container fluid>{children}</Container>
       </div>
     );
   }
