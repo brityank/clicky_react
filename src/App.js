f4f0c58acd1fbcb702c2bcb4a861ffedf46b2ac3
Aaron Keisler <amkeisler@gmail.com>
2019-09-26 20:31:30 -0400

    fix issues with score

--- src/App.js
+++ src/App.js
@@ -4,36 +4,72 @@ import Body from "./components/Body";
 import Clicky from "./components/Clicky";
 import Nav from "./components/Navbar";
 import imageList from "./data/imageList";
-// import _ from "underscore";
+import { shuffle } from "underscore";
 
 export default class App extends React.Component {
   state = {
     currentScore: 0,
     highScore: 0,
-    shake: false,
     usrMessage: "Don't click on the same gif twice!",
     images: imageList,
-    clicked: []
+    clicked: [],
+    winning: true,
+    shake: false
   };
 
   handleClick(e) {
+    // console.log(this.state);
     const newState = { ...this.state };
-    newState.clicked.push(e.target.alt);
-    newState.images = _.shuffle(newState.images);
-    this.setState({ newState });
+    const choice = e.target.alt;
+    newState.clicked.push(choice);
+    if (new Set(newState.clicked).size < newState.clicked.length) {
+      newState.usrMessage = "Oh well. Try again!";
+      newState.winning = false;
+      newState.shake = true;
+    } else {
+      newState.currentScore++;
+      newState.highScore = newState.highScore < newState.currentScore ? newState.currentScore : newState.highScore;
+      newState.usrMessage = "Correct! Keep going!";
+      newState.winning = true;
+      newState.shake = false;
+    }
+    newState.images = shuffle(newState.images);
+    // console.log(newState);
+    this.setState({ ...newState });
+  }
+
+  componentDidUpdate() {
+    if (this.state.winning === false) {
+      const restartState = {
+        highScore: this.state.highScore,
+        usrMessage: "Play again! Don't click on the same gif twice.",
+        currentScore: 0,
+        images: imageList,
+        clicked: [],
+        winning: true,
+        shake: true
+      };
+      this.setState({ ...restartState });
+    }
   }
 
   render() {
-    const { currentScore, highScore, shake, usrMessage, images } = this.state;
+    const { currentScore, highScore, shake, usrMessage, images, winning } = this.state;
     return (
       <div>
         <Nav score={currentScore} highScore={highScore} msg={usrMessage} />
-        <Body>
-          <div className={shake ? "grid animated shake" : "grid"}>
-            {images.map((image, i) => {
-              return <Clicky src={image.src} alt={image.alt} key={i} handleClick={this.handleClick.bind(this)} />;
-            })}
-          </div>
+        <Body shake={shake}>
+          {images.map((image, i) => {
+            return (
+              <Clicky
+                src={image.src}
+                alt={image.alt}
+                key={i}
+                clickEvent={this.handleClick.bind(this)}
+                flip={winning}
+              />
+            );
+          })}
         </Body>
       </div>
     );
