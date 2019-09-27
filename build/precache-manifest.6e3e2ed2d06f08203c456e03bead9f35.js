c5884be4d84d39f53d487b0f0e29704c89374e59
Aaron Keisler <amkeisler@gmail.com>
2019-09-26 22:05:15 -0400

    create build

---
+++ build/precache-manifest.6e3e2ed2d06f08203c456e03bead9f35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b942633509ffbf8d90309b16b28fe3ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "963f37667ce8ed5b1aaf",
+    "url": "/static/css/main.9516b28e.chunk.css"
+  },
+  {
+    "revision": "a6b54f8cba42bb0f8591",
+    "url": "/static/js/2.dc19a1fa.chunk.js"
+  },
+  {
+    "revision": "963f37667ce8ed5b1aaf",
+    "url": "/static/js/main.82a9d655.chunk.js"
+  },
+  {
+    "revision": "79a6e15bd7761883beac",
+    "url": "/static/js/runtime-main.b8d0c4cc.js"
+  }
+]);
