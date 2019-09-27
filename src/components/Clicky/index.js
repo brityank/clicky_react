7a987b94125d6eb1770da01377b27f20acf7cfc7
Aaron Keisler <amkeisler@gmail.com>
2019-09-26 20:31:53 -0400

    add flip for open

--- src/components/Clicky/index.js
+++ src/components/Clicky/index.js
@@ -3,8 +3,14 @@ import { Image } from "react-bootstrap";
 import "./style.css";
 
 export default class Clicky extends Component {
+  componentDidMount() {
+    if (this.props.flip) {
+      document.querySelectorAll(".rounded").forEach(el => el.classList.add("animated", "flipInX"));
+    }
+  }
+
   render() {
     const { src, alt, clickEvent, id } = this.props;
-    return <Image src={src} alt={alt} onClick={clickEvent} key={id} rounded />;
+    return <Image src={src} alt={alt} onClick={clickEvent} key={id} className="rounded" />;
   }
 }
